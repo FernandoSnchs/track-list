def23a947549d70aab949aae609767fc60caf897
Fernando <fernando.sanches.rodrigues@outlook.com>
2025-08-16 17:48:13 -0300

    v 1.0.2

--- js.js
+++ js.js
@@ -3,48 +3,50 @@ const addButton = document.getElementById('Add-B');
 const RemoveButton = document.getElementById('Rem-B');
 const taskInput = document.getElementById('task-input');
 const listaContainer= document.querySelector('.lista-tarefas');
-const tarefa = document.querySelector('tarefa');
+
 let contador = 0;
 //
 addButton.addEventListener('click', function() {
+    const textoTarefa = taskInput.value;
+    if (textoTarefa.trim() === "") return;
     contador++;
     atualizarContador();
-    const textoTarefa = taskInput.value;
     const novaTarefa = document.createElement("li");
+    novaTarefa.classList.add('tarefa'); 
     const lista = document.querySelector('.lista-tarefas ul');
     lista.appendChild(novaTarefa);
     taskInput.value = '';//apaga o placehoder anterior
 
     const textoSpan = document.createElement("span");//cria o texto da tarefa
     textoSpan.textContent = textoTarefa;//coloca o texto
-    lista.appendChild(textoSpan);
+    novaTarefa.appendChild(textoSpan);
 
     const botaoconcluir = document.createElement("button");
-    botaoconcluir.textContent = "V";
-    lista.appendChild(botaoconcluir);
+    botaoconcluir.innerHTML = '<i class="fas fa-check"></i>'; // ícone de check
+    botaoconcluir.classList.add("icone-botao");
+    novaTarefa.appendChild(botaoconcluir);
 
     const botaoexcluir = document.createElement("button");
-    botaoexcluir.textContent = "X";
-    lista.appendChild(botaoexcluir);
+    botaoexcluir.innerHTML = '<i class="fas fa-trash"></i>'; // ícone de lixeira
+    botaoexcluir.classList.add("icone-botao");
+    
+    novaTarefa.appendChild(botaoexcluir);
 
             //quando clicar no botao de excluir
-    botaoexcluir.addEventListener('click', function(){
-            textoSpan.remove();
-            botaoexcluir.remove();
-            botaoconcluir.remove();
+   botaoexcluir.addEventListener('click', function(){            
             novaTarefa.remove();
             contador--;
             atualizarContador();
-    });
+    }); 
 
     //botaoconcluir
     botaoconcluir.addEventListener('click', function(){
         textoSpan.classList.add("concluido");
          // Aguarda 3 segundos (3000 milissegundos) e remove tudo
     setTimeout(function () {
-        textoSpan.remove();
-            botaoexcluir.remove();
-            botaoconcluir.remove();
+            textoSpan.style.opacity = "0.5";
+            novaTarefa.style.transition = "opacity 1.2s";
+            novaTarefa.style.opacity = "0.5";
             novaTarefa.remove();
             contador--;
             atualizarContador();
